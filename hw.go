836a67cea2e77217066f33a5648e833a9a21ef18
NewNes25 <broodway@gmail.com>
2025-02-10 03:48:03 +0000

    Serve client.html

--- hw.go
+++ hw.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"sync"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -22,52 +21,35 @@ type Player struct {
 }
 
 var players []*Player
-var mutex sync.Mutex
 
 func handleConnection(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("Error upgrading connection:", err)
+		log.Println("WebSocket upgrade error:", err)
 		return
 	}
 	defer conn.Close()
 
 	player := &Player{conn: conn}
-	mutex.Lock()
 	players = append(players, player)
-	playerID := len(players)
-	mutex.Unlock()
 
-	log.Printf("Player %d connected\n", playerID)
-
-	// Wait for the player to send a number (1 or 2)
+	// Wait for number
 	_, msg, err := conn.ReadMessage()
 	if err != nil {
-		log.Println("Error reading message:", err)
-		removePlayer(conn)
+		log.Println("Read error:", err)
 		return
 	}
 
-	var num int
-	if msg[0] == '1' {
-		num = 1
-	} else if msg[0] == '2' {
-		num = 2
-	} else {
+	if msg[0] != '1' && msg[0] != '2' {
 		conn.WriteMessage(websocket.TextMessage, []byte("Invalid input! Send '1' or '2'."))
-		removePlayer(conn)
 		return
 	}
+	player.number = int(msg[0] - '0')
 
-	player.number = num
-	log.Printf("Player %d chose: %d\n", playerID, num)
-
-	// Wait until two players are connected
-	mutex.Lock()
+	// Check if two players are connected
 	if len(players) == 2 {
 		determineWinner()
 	}
-	mutex.Unlock()
 }
 
 func determineWinner() {
@@ -81,28 +63,27 @@ func determineWinner() {
 		result = "Player 1 wins!"
 	}
 
-	// Send results to both players
-	p1.conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("You chose %d, Opponent chose %d. %s", p1.number, p2.number, result)))
-	p2.conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("You chose %d, Opponent chose %d. %s", p2.number, p1.number, result)))
+	// Send results to players
+	p1.conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("You: %d, Opponent: %d. %s", p1.number, p2.number, result)))
+	p2.conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("You: %d, Opponent: %d. %s", p2.number, p1.number, result)))
 
-	// Reset for the next round
+	// Reset players for the next round
 	players = []*Player{}
 }
 
-func removePlayer(conn *websocket.Conn) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	for i, p := range players {
-		if p.conn == conn {
-			players = append(players[:i], players[i+1:]...)
-			break
-		}
-	}
-}
-
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Serve static files (client.html)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	// Handle WebSocket connections
 	http.HandleFunc("/ws", handleConnection)
-	port := "8080"
-	log.Println("WebSocket server running on port", port)
+
+	log.Println("Server running on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
